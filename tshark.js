e0b9cb7db986ea9e4b52503f48861260cbe02552
luis-puhl <luispuhl@gmail.com>
2017-01-23 06:38:41 -0200

    Refactor tshark.js

--- tshark.js
+++ tshark.js
@@ -1,142 +1,130 @@
 const csv = require("fast-csv");
 const EventEmitter = require("events");
 
-let devices = {};
+let devices = {};	// lista indexada de dispositivos
 
 const emitterInstance = new EventEmitter();
+
+/* ----------------------------------------------------------------------- */
 
 class Device {
 	constructor(mac, macResolved){
 		this.mac = mac;
 		this.macResolved = macResolved;
-		this.rssHistory = {};
+		this.rssHistory = [];
 		this.ssidHistory = {};
-		this.taHistory = [];
-		emitterInstance.emit('newDevice', this);
+		this.taHistory = {};
 	}
-	updateRssStatistics(size, avg, variance, stdDeviation){
-		this.rssStatistics = {
-			size			: size,
+	get rssStatistics(){
+		let sum = 0;
+		let avg = 0;
+		let variance = 0;
+		let stdDeviation = 0;
+		if (this.rssHistory.length > 0){
+			for (let rss of this.rssHistory){
+				sum += rss;
+			}
+			avg = sum / this.rssHistory.length;
+
+			sum = 0;
+			for (let rss of this.rssHistory){
+				sum += Math.pow(( rss - avg ), 2);
+			}
+			variance = sum / (this.rssHistory.length - 1);
+			stdDeviation = Math.sqrt(variance);
+		}
+		return {
+			size			: this.rssHistory.length,
 			avg				: avg,
 			variance		: variance,
 			stdDeviation	: stdDeviation,
-			time			: (new Date()).toISOString(),
+			time			: ( new Date() ).toISOString(),
 		};
 	}
 	toJSON() {
-		let rssHistory = {};
-		for (let rss in this.rssHistory){
-			rssHistory[rss] = this.rssHistory[rss];
-		}
 		let ssidHistory = {};
 		for (let ssid in this.ssidHistory){
 			ssidHistory[ssid] = this.ssidHistory[ssid];
 		}
+		let taHistory = {};
+		for (let ta in this.taHistory){
+			taHistory[ta] = this.taHistory[ta];
+		}
 		return {
-			"mac"			: this.mac,
-			"macResolved"	: this.macResolved,
-			"rssHistory"	: rssHistory,
-			"ssidHistory"	: ssidHistory,
-			"rssStatistics"	: this.rssStatistics,
+			mac				: this.mac,
+			macResolved		: this.macResolved,
+			rssHistory		: this.rssHistory,
+			ssidHistory		: ssidHistory,
+			taHistory		: taHistory,
+			rssStatistics	: this.rssStatistics,
+		};
+	}
+	appendPacket(packet){
+		let rss = packet.radiotap.dbm_antsignal;
+		let number = parseInt( rss.split(",")[0].split('-')[1] );
+		this.rssHistory.push(number);
+
+		let curTime = ( new Date() ).toISOString();
+		this.ssidHistory[packet.wlan_mgt.ssid] = curTime;
+		this.taHistory[packet.wlan.ta] = {
+			ta			: packet.wlan.ta,
+			ta_resolved	: packet.wlan.ta_resolved,
 		};
 	}
 }
 
 /* ----------------------------------------------------------------------- */
 
-function updateDevices(data) {
-	let wlan = {
-		sa				: data[0], // sender address
-		sa_resolved		: data[1], // sender address resolved
-		ta				: data[2], // transmitter address
-		ta_resolved		: data[3], // transmitter address resolved
-	};
-	let radiotap = {
-		dbm_antsignal	: data[4],
-	};
-	let wlan_mgt = {
-		ssid			: data[5],
-	};
-
-	let curTime = (new Date()).toISOString();
-	if (! devices[wlan.sa]){
-		devices[wlan.sa] = new Device(wlan.sa, wlan.sa_resolved);
+class Packet {
+	constructor(sa, sa_resolved, ta, ta_resolved, dbm_antsignal, ssid){
+		this.wlan = {
+			sa				: sa,				// sender address
+			sa_resolved		: sa_resolved,		// sender address resolved
+			ta				: ta, 				// transmitter address
+			ta_resolved		: ta_resolved,		// transmitter address resolved
+		};
+		this.radiotap = {
+			dbm_antsignal	: dbm_antsignal,	// potencia de sinal (rss)
+		};
+		this.wlan_mgt = {
+			ssid			: ssid, 			// nome da rede no pacote Beacon
+		};
 	}
-	devices[wlan.sa].rssHistory[curTime] = radiotap.dbm_antsignal;
-	devices[wlan.sa].ssidHistory[wlan_mgt.ssid] = curTime;
-	devices[wlan.sa].taHistory.push({'ta': wlan.ta, 'ta_resolved': wlan.ta_resolved});
 }
 
-/*Math functions -------------------------------------------------------------*/
-function arrayAvg(array){ //Avg function
-	let sum = 0;
-	for(let i = 0; i < array.length; i++) {
-		sum += array[i];
-	}
-	return sum / array.length;
-}
-function arrayVariance(array, avg){//variance function
-	let sum = 0;
-	for(let i = 0; i < array.length; i++){
-		sum += Math.pow(( array[i] - avg ), 2);
-	}
-	return sum / (array.length - 1);
-}
+/* ----------------------------------------------------------------------- */
 
-function updatedeDeviceStatistics() {
-	let rssDevices = [];
-	let devicesCout = 0;
-	for (let deviceKey in devices){
-		let device = devices[deviceKey];
-		let rssArray = [];
-		let rssHistoryLength = 0;
-		devicesCout++;
-		for (let rssKey in device.rssHistory){
-			let rss = device.rssHistory[rssKey];
-			rssHistoryLength++;
-			// get first integer from each Rss string on rssHistory
-			let number = parseInt(rss.split(",")[0].split('-')[1]);
-			// list of rss for a device
-			rssArray.push(number);
-			// list of rss from all devices
-			rssDevices.push(number);
-		}
-		//statistics for a device
-		let avg = arrayAvg(rssArray);
-		let variance = arrayVariance(rssArray, avg);
-		let std = Math.sqrt(variance);
-		device.updateRssStatistics(rssHistoryLength, avg, variance, std);
+function processarPacote(packet) {
+	let sa = packet.wlan.sa;
+	if (! devices[sa]){
+		devices[sa] = new Device(sa, packet.wlan.sa_resolved);
+		emitterInstance.emit('newDevice', devices[sa]);
 	}
-	//statistics for all devices
-	let avgDevices = arrayAvg(rssDevices);
-	let varianceDevices = arrayVariance(rssDevices, avgDevices);
-	let std = Math.sqrt(varianceDevices);
-	return {
-		devicesCout					: devicesCout,
-		overallAverage				: avgDevices,
-		overallStandardDeviation	: std,
-	}
+	devices[sa].appendPacket(packet);
 }
 
 /* ----------------------------------------------------------------------- */
 
 function getReport(){
-	let statistics = updatedeDeviceStatistics();
-	return {
-		devicesCout					: statistics.devicesCout,
-		overallAverage				: statistics.avgDevices,
-		overallStandardDeviation	: statistics.std,
+	let devicesCout = 0;
+	let totalRss = 0;
+	let totalPackets = 0;
+
+	for (let deviceKey in devices){
+		let device = devices[deviceKey];
+		devicesCout++;
+
+		let deviceRss = device.rssStatistics;
+
+		totalRss += deviceRss.size * deviceRss.avg;
+		totalPackets += deviceRss.size;
 	}
-}
-
-function getDeviceReport(macAddress) {
-	updatedeDeviceStatistics();
-	return devices[macAddress];
-}
-
-function getDevices() {
-	updatedeDeviceStatistics();
-	return devices;
+	//statistics for all devices
+	let avgDevices = totalRss / totalPackets;
+	return {
+		devicesCout		: devicesCout,
+		overallAverage	: avgDevices,
+	}
 }
 
 /* ----------------------------------------------------------------------- */
@@ -199,23 +187,14 @@ function spawnTshark(){
 			'-e', 'wlan_mgt.ssid',
 			'-Y', 'wlan.sa'
 		]);
-	tsharkChild.stdout.setEncoding('utf8');
 	return tsharkChild;
 }
 
 /* ----------------------------------------------------------------------- */
 
-let csvStream = csv()
-	.on("data", function(data){
-		updateDevices(data);
-	})
-	.on("end", function(){
-		console.log("done with tshark");
-	});
-
-/* ----------------------------------------------------------------------- */
-
 let tsharkChild;
+let csvStream;
+
 function shutdown(){
 	try {
 		if (csvStream == undefined){
@@ -233,6 +212,22 @@ function shutdown(){
 
 module.exports = () => {
 
+	csvStream = csv()
+		.on("data", function(data){
+			let packet = new Packet(
+				data[0], // sender address
+				data[1], // sender address resolved
+				data[2], // transmitter address
+				data[3], // transmitter address resolved
+				data[4], // potencia de sinal (rss)
+				data[5], // nome da rede no pacote Beacon
+			);
+			processarPacote(packet);
+		})
+		.on("end", function(){
+			console.log("done with csv");
+		});
+
 	tsharkChild = spawnTshark();
 
 	tsharkChild.stderr.on('data', (data) => {
@@ -243,13 +238,14 @@ module.exports = () => {
 		console.log(`child process exited with code ${code}`);
 	});
 
+	tsharkChild.stdout.setEncoding('utf8');
 	tsharkChild.stdout.pipe(csvStream);
 
 	return {
-		getDevices			: getDevices,
 		emitterInstance		: emitterInstance,
 		shutdown			: shutdown,
 		getReport			: getReport,
-		getDeviceReport		: getDeviceReport,
+		getDevices			: () => {return devices;},
+		getDeviceReport		: (macAddress) => {return devices[macAddress];},
 	};
 }
